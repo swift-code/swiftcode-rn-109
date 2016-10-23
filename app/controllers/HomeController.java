523166c595afc634fa79aa7f87d150dd745c4fc0
Live session user <sandeep.k@betsol.com>
2016-10-23 10:24:19 +0000

    fourth

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -5,12 +5,8 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.ConnectionRequest;
 import models.Profile;
 import models.User;
-import play.data.validation.ValidationError;
-import play.db.ebean.Model;
 import play.mvc.Controller;
 import play.mvc.Result;
-import sun.net.ConnectionResetException;
-import sun.rmi.transport.Connection;
 
 import javax.inject.Inject;
 import java.util.stream.Collectors;
@@ -40,6 +36,28 @@ public class HomeController extends Controller {
            connectionJson.put("company",connectionProfile.company);
            return (connectionJson);
        }).collect(Collectors.toList())));
-       return ok();
+
+       data.set("connectionRequests", objectMapper.valueToTree(user.connectionRequestsReceived.stream().filter(x->x.status.equals(ConnectionRequest.Status.WAITING)).map(connectionRequest->{
+           ObjectNode connectionRequestJson = objectMapper.createObjectNode();
+           Profile connectionRequestProfile= Profile.find.byId(connectionRequest.sender.profile.pid);
+           connectionRequestJson.put("id", connectionRequest.cid);
+           connectionRequestJson.put("firstName", connectionRequestProfile.fname);
+           return (connectionRequestJson);
+       }).collect(Collectors.toList())));
+
+       data.set("connectionRequests", objectMapper.valueToTree(User.find.all().stream()
+               .filter(x->!user.equals(x))
+               .filter(x->!user.connections.contains(x))
+               .filter(x->!user.connectionRequestsReceived.stream().map(y->y.sender).collect(Collectors.toList()).contains(x))
+               .filter(x->!user.connectionRequestsSent.stream().map(y->y.receiver).collect(Collectors.toList()).contains(x))
+               .map(x->{
+                   ObjectNode userJson = objectMapper.createObjectNode();
+                   Profile suggestionProfile = Profile.find.byId(x.profile.pid);
+                   userJson.put("id", x.uid);
+                   userJson.put("firstName", suggestionProfile.fname);
+                   return (userJson);
+               })
+               .collect(Collectors.toList())));
+       return ok(data);
    }
 }
--- app/forms/LoginForm.java
+++ app/forms/LoginForm.java
@@ -1,48 +1,29 @@
-package forms;
 
-import models.User;
+package forms;import models.User;
 import org.mindrot.jbcrypt.BCrypt;
 import play.data.validation.Constraints;
-import play.data.validation.ValidationError;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
+import play.data.validation.ValidationError;import java.util.ArrayList;
+import java.util.List;/**
  * Created by lubuntu on 10/22/16.
  */
-public class LoginForm {
-    @Constraints.Required
-    public String email;
-
-    @Constraints.Required
-    public String password;
-
-    public List<ValidationError> validate(){
-        List<ValidationError> errors = new ArrayList<>();
-        User appUser = User.find.where().eq("email",email).findUnique();
-
-        if(appUser == null)
-            errors.add(new ValidationError("message","Incorrect user name or password"));
-        else if(!BCrypt.checkpw(password,appUser.password))
-            errors.add(new ValidationError("message","Incorrect user name or password"));
-
-        return errors;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public class LoginForm {    @Constraints.Required
+public String email;    @Constraints.Required
+public String password;    public List<ValidationError> Validate()
+{
+    List<ValidationError>  errors = new ArrayList<>();
+    User user= User.find.where().eq("email", email).findUnique();
+    if(user==null)
+        errors.add(new ValidationError("message","Incorrect email or password"));
+    else if(!BCrypt.checkpw(password, user.password))
+        errors.add(new ValidationError("message","Incorrect email or password"));
+    return errors;
+}    public String getEmail() {
+    return email;
+}    public String getPassword() {
+    return password;
+}    public void setEmail(String email) {
+    this.email = email;
+}    public void setPassword(String password) {
+    this.password = password;
+}
 }
